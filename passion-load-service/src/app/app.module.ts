92ba9ec01653da6ce1e0f04fc68d5c2da60dbe59
seoyeon-jung <jungluchia1204@gmail.com>
2026-02-10 16:31:51 +0900

    feat: module에 middleware 적용 (#1)

--- passion-load-service/src/app/app.module.ts
+++ passion-load-service/src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health.controller';
+import { OrgMiddleware } from '../common/middlewares/org.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(OrgMiddleware).forRoutes('*');
+  }
+}
