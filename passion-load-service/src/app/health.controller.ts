6d99daf09fe1f2255830ed6e8305dd939298357d
seoyeon-jung <jungluchia1204@gmail.com>
2026-02-12 15:38:12 +0900

    chore: format 적용

--- passion-load-service/src/app/health.controller.ts
+++ passion-load-service/src/app/health.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
-import { PrismaService } from "../common/prisma/prisma.service";
+import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from '../common/prisma/prisma.service';
 
 @Controller()
 export class HealthController {
-    // prisma 주입해서 테스트 [추후 필요시 삭제]
-    constructor(private readonly prisma: PrismaService) {}
+  // prisma 주입해서 테스트 [추후 필요시 삭제]
+  constructor(private readonly prisma: PrismaService) {}
 
-    @Get('health')
-    async getHealth() {
-        await this.prisma.$queryRaw`SELECT 1`;
-        return { ok: true };
-    }
+  @Get('health')
+  async getHealth() {
+    await this.prisma.$queryRaw`SELECT 1`;
+    return { ok: true };
+  }
 }
