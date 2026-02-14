824aa1d24aa2e4e8478562f6c973972fc071dd62
seoyeon-jung <jungluchia1204@gmail.com>
2026-02-14 15:05:49 +0900

    fix: 서버 오류 해결

--- passion-load-service/src/common/prisma/prisma.service.ts
+++ passion-load-service/src/common/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
