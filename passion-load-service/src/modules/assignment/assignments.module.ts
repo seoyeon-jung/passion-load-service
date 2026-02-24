26fc9b9ec1c85504aa65544bb8effedc8ce6616d
seoyeon-jung <jungluchia1204@gmail.com>
2026-02-24 18:03:01 +0900

    feat: assignments module에 dailyCheck 추가 (#16)

--- passion-load-service/src/modules/assignment/assignments.module.ts
+++ passion-load-service/src/modules/assignment/assignments.module.ts
@@ -2,10 +2,11 @@ import { PersistenceModule } from '@modules/persistence.module';
 import { Module } from '@nestjs/common';
 import { AssignmentsController } from './adapters/in/assignments.controller';
 import { AssignmentService } from './assignments.service';
+import { DailyChecksController } from './adapters/in/daily-checks.controller';
 
 @Module({
   imports: [PersistenceModule],
-  controllers: [AssignmentsController],
+  controllers: [AssignmentsController, DailyChecksController],
   providers: [AssignmentService],
 })
 export class AssignmentModule {}
