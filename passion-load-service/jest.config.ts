e3cabaa55c7e803f252ebc495861d51a29ea0397
seoyeon-jung <jungluchia1204@gmail.com>
2026-03-05 13:19:40 +0900

    test: Jest 기반 NX test 환경 설정 (#20)

---
+++ passion-load-service/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  displayName: 'passion-load-service',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.app.json' }],
+  },
+  moduleNameMapper: {
+    '^@common/(.*)$': '<rootDir>/src/common/$1',
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+  },
+};
+
+export default config;
